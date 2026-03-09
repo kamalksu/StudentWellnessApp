69765b3795b4a90cfe3127826420ed25578c8427
kamalksu <zkamal@students.kennesaw.edu>
2026-03-09 17:59:20 -0400

    updated gitignore

--- firebase/config.js
+++ firebase/config.js
@@ -7,12 +7,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAzoXMpBcMS9GhMy_s4a8nqbN_4PRXSePI",
-  authDomain: "studentwellnessapp-bf2e1.firebaseapp.com",
-  projectId: "studentwellnessapp-bf2e1",
-  storageBucket: "studentwellnessapp-bf2e1.firebasestorage.app",
-  messagingSenderId: "486003488553",
-  appId: "1:486003488553:web:128bd20f270067d89e6938"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
